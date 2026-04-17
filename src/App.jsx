ac59baf0b86e2bb7b32079e0253be26db052ca9e
Aayan Khan <aayan.khan02072007@gmail.com>
2026-04-17 19:25:58 +0530

    Removed Team View for now

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,15 @@ import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [listView, setListView] = useState(true);
-  const [teamView, setTeamView] = useState(false);
   return (
     <>
       <div id="title_container" className="centered">
         <h1 id="title">Genshin Tierlist</h1>
       </div>
       <div id="selector" className="centered">
-        <button
-          id="list"
-          onClick={() => {
-            setListView(true);
-            setTeamView(false);
-          }}
-        >
-          List Format
-        </button>
-        <button
-          id="team"
-          onClick={() => {
-            setListView(false);
-            setTeamView(true);
-          }}
-        >
-          Teams Format
-        </button>
       </div>
       <div id="viewing_area">
         {listView ? <ListView /> : ""}
-        {teamView ? <TeamView /> : ""}
       </div>
       <footer>
         This content is not affiliated with Hoyoverse or its subsidiaries.
