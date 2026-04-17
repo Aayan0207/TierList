5742beae80fda658b4b0b650e01b5162568f6056
Aayan Khan <aayan.khan02072007@gmail.com>
2026-04-17 20:02:56 +0530

    Removed teams for now

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import ListView from "./components/ListView";
 import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  const [listView, setListView] = useState(true);
   return (
     <>
       <div id="title_container" className="centered">
@@ -14,7 +13,7 @@ function App() {
       <div id="selector" className="centered">
       </div>
       <div id="viewing_area">
-        {listView ? <ListView /> : ""}
+         <ListView />
       </div>
       <footer>
         This content is not affiliated with Hoyoverse or its subsidiaries.
